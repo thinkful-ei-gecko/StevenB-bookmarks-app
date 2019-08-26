8b4f627c3e8bd5bbd42dd46006358c65d3fa4444
Steven Bull <stevenwbulljr@gmail.com>
2019-08-26 11:46:13 -0500

    added pseudocode to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+//will filter bookmarks currently in DOM
+//filter based on 1-5 rating system
+function filterBookmarksWasClicked() {}
+
+//will generate form to add a new bookmark to the DOM
+function addBookmarkClicked() {}
+
+//will watch the add bookmark form
 function watchForm() {
   console.log('I\'m running!');
   $('#js-bookmark-form').submit( function(event) {
@@ -9,5 +17,8 @@ function watchForm() {
     console.log(`You entered the name: ${websiteName} for: ${websiteURL}!`);
   });
 }
+
+//will render the added information onto the DOM
+function renderPage() {}
 
 $(watchForm);
